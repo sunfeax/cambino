d60a44bedc17f6712ad12d5a33e61d0c7528cf08
sunfeax <sunfeax@gmail.com>
2025-10-29 09:47:56 +0100

    add styles with AI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Home } from './components/home/home';
 import { Landing } from './components/landing/landing';
 import { Ej01clase } from './components/ej01clase/ej01clase';
 import { SaludoEnrutado } from './components/saludoEnrutado/saludoEnrutado';
+import { RxJS } from './components/rxjs/rxjs';
 
 // aquí añadimos nombres y asignamos componento de las rutas (p.e.: localhost:4200/home
 export const routes: Routes = [
@@ -11,6 +12,8 @@ export const routes: Routes = [
     {path: 'ej01', component: Ej01clase},
     {path: 'saludo_enrutado', component: SaludoEnrutado},
     {path: 'saludo_enrutado/:nombre', component: SaludoEnrutado},
-    {path: '', component: Home}, // Ruta por defecto
-    {path: '**', redirectTo: 'home'}, // Redirigir cualquier otra ruta a home
+    {path: 'rxjs', component: RxJS},
+
+    {path: '', component: Home}, // ruta por defecto
+    {path: '**', redirectTo: 'home'}, // redirigir cualquier otra ruta a home
 ];
---
+++ src/app/components/rxjs/rxjs.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-rxjs',
+  imports: [RouterModule],
+  templateUrl: './rxjs.html',
+  styleUrl: './rxjs.css',
+  standalone: true
+})
+
+export class RxJS {
+
+  // const nombres$: Observable
+
+}
