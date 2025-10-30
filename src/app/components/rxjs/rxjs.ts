d7bc577194f495d7e754d5ebed661c03b773d9b4
sunfeax <sunfeax@gmail.com>
2025-10-30 09:01:28 +0100

    Ejemplos con filter, map, take, unsubscribe, Subject, BehaviorSubject

--- src/app/components/rxjs/rxjs.ts
+++ src/app/components/rxjs/rxjs.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { interval, merge, Observable, of, take } from 'rxjs';
+import { BehaviorSubject, filter, interval, map, merge, Observable, of, Subject, take } from 'rxjs';
 
 class Persona {
   nombre: string = '';
@@ -65,7 +65,7 @@ export class RxJS {
 
     this.numero$.subscribe({
       next: (numero: number) => {
-        console.log('Número recibido: ' + numero);
+        console.log(`Número recibido: ${numero}`);
       },
       error: (err) => {
         console.error('Error al recibir el número:', err);
@@ -130,7 +130,7 @@ export class RxJS {
   }
 
   ejemplo06() {
-    console.log();
+    console.log('Ejecutando ejemplo 06 de RxJS: merge/pipe/take');
 
     const interval1$ = interval(1000);
     const interval2$ = interval(1500);
@@ -146,5 +146,132 @@ export class RxJS {
         console.log('Flujo de datos completado.');
       }
     })
+  }
+
+  ejemplo07() {
+    console.log('Ejecutando ejemplo 06 de RxJS: filter');
+
+    const interval1$ = interval(810);
+    const interval2$ = interval(340);
+
+    const combinado1$ = merge(interval1$, interval2$);
+    const combinado2$ = combinado1$.pipe(
+      take(20),
+      filter(num => num % 2 == 0)
+    )
+
+    combinado2$.subscribe({
+      next: numero => console.log(`Numero recibido: ${numero}`),
+      error: (err) => {console.error('Error al recibir el número:', err,)},
+      complete: () => {console.log('Flujo de datos completado.')}
+    })
+  }
+
+  ejemplo08() {
+    console.log('Ejecutando ejemplo 06 de RxJS: map');
+
+    const interval1$ = interval(810);
+    const interval2$ = interval(340);
+
+    const combinado1$ = merge(interval1$, interval2$);
+    const combinado2$ = combinado1$.pipe(
+      take(20),
+      filter(num => num % 2 == 0),
+      map(num => num * 10)
+    )
+
+    combinado2$.subscribe({
+      next: numero => console.log(`Numero recibido: ${numero}`),
+      error: (err) => {console.error('Error al recibir el número:', err,)},
+      complete: () => {console.log('Flujo de datos completado.')}
+    })
+  }
+
+  ejemplo09() {
+    console.log('Ejecutando ejemplo 10 de RxJS: subscription con 2 observables y unsubscribe');
+
+    // Crea un Observable que emite un valor cada 500 ms
+    const intervalo$ = interval(500);
+
+    // Suscriptor 1
+    const subscription1 = intervalo$.subscribe({
+      next: numero => console.log('Suscriptor 1 - Número recibido: ' + numero),
+      error: err => console.error('Suscriptor 1 - Error al recibir el número:', err),
+      complete: () => console.log('Suscriptor 1 - Flujo de datos completado.')
+    });
+
+    // Suscriptor 2
+    const subscription2 = intervalo$.subscribe({
+      next: numero => console.log('Suscriptor 2 - Número recibido: ' + numero),
+      error: err => console.error('Suscriptor 2 - Error al recibir el número:', err),
+      complete: () => console.log('Suscriptor 2 - Flujo de datos completado.')
+    });
+
+    // Desuscribirse de sub1 después de 5000 ms para detener la emisión
+    setTimeout(() => {
+      subscription1.unsubscribe();
+      console.log('Desuscrito del observable de intervalo el suscriptor 1.');
+    }, 5000);
+
+    // Desuscribirse de sub2 después de 10000 ms para detener la emisión
+    setTimeout(() => {
+      subscription2.unsubscribe();
+      console.log('Desuscrito del observable de intervalo el suscriptor 1.');
+    }, 10000);
+  }
+
+  ejemplo10() {
+    console.log('Ejecutando ejemplo 10 de RxJS: Subject');
+
+    // Crea un nuevo Subject que emitirá strings
+    const subject = new Subject<string>();
+
+    // Suscriptor 1 (s1): Se suscribe y recibirá todos los valores futuros
+    subject.subscribe({
+        next: (value) => console.log('s1: Valor recibido en el subject: ' + value),
+        complete: () => console.log('s1: Flujo de datos completado en el subject.')
+    });
+
+    // Emite dos valores. s1 los recibe.
+    subject.next('Hola');
+    subject.next('Mundo');
+
+    // Suscriptor 2 (s2): Se suscribe *después* de que 'Hola' y 'Mundo' fueron emitidos.
+    // Un Subject normal NO reemite valores anteriores, por lo que s2 solo recibirá
+    // los valores que se emitan a partir de este punto.
+    subject.subscribe({
+        next: (value) => console.log('s2: Valor recibido en el subject: ' + value),
+        complete: () => console.log('s2: Flujo de datos completado en el subject.')
+    });
+
+    // Emite un valor. s1 y s2 lo reciben.
+    subject.next('Saludos desde el Subject!');
+
+    // Completa el flujo de datos. s1 y s2 reciben la notificación 'complete'.
+    subject.complete();
+  }
+
+  ejemplo11() {
+    console.log('Ejecutando ejemplo 11 de RxJS: BehaviorSubject');
+
+    // Crea un nuevo BehaviorSubject que emitirá strings
+    const subject = new BehaviorSubject<string>('Valor inicial');
+
+    subject.subscribe({
+        next: (value) => console.log('s1: Valor recibido en el subject: ' + value),
+        complete: () => console.log('s1: Flujo de datos completado en el subject.')
+    });
+
+    subject.next('Hola');
+    subject.next('Mundo');
+
+    subject.subscribe({
+        next: (value) => console.log('s2: Valor recibido en el subject: ' + value),
+        complete: () => console.log('s2: Flujo de datos completado en el subject.')
+    });
+
+    subject.next('Saludos desde el Subject!');
+
+    subject.complete();
   }
 }
---
+++ src/app/padre/padre.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-padre',
+  imports: [],
+  templateUrl: './padre.html',
+  styleUrl: './padre.css',
+})
+export class Padre {
+
+}
